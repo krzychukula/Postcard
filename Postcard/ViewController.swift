52a8239b749832070ae4badc1753736ace604374
krzychukula <krzychukula@gmail.com>
2014-12-07 11:46:43 +0100

    connect button

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
     
 
     override func viewDidLoad() {
@@ -23,6 +25,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+    }
 
 }
 
